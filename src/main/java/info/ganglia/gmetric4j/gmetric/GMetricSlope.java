61133447ad70fde2d0f80e2db52394bdb7622a87
wangjl <2627812260@qq.com>
2016-10-13 20:18:02 +0800

    add derivative slope

--- src/main/java/info/ganglia/gmetric4j/gmetric/GMetricSlope.java
+++ src/main/java/info/ganglia/gmetric4j/gmetric/GMetricSlope.java
@@ -10,11 +10,13 @@ package info.ganglia.gmetric4j.gmetric;
  * @author humphrej
  */
 public enum GMetricSlope {
-    //zero|positive|negative|both
+    //zero|positive|negative|both|derivative
     ZERO(0),
     POSITIVE(1),
     NEGATIVE(2),
-    BOTH(3);
+    BOTH(3),
+    UNSPECIFIED(4),
+    DERIVATIVE(5);
     
     private int gangliaSlope ;
     
